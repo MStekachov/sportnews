e9f935618b9a81766357ff73a33da4f3c5b98553
Михаил Стекачёв <mstekachov@gmail.com>
2022-02-28 02:41:30 +0300

    gii-generated news unit

---
+++ models/News.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "news".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property string $content
+ * @property int|null $status
+ * @property string|null $created_dt
+ * @property string|null $updated_dt
+ * @property string|null $published_dt
+ * @property string|null $comments
+ */
+class News extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'news';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'description', 'content'], 'required'],
+            [['description', 'content', 'comments'], 'string'],
+            [['status'], 'integer'],
+            [['created_dt', 'updated_dt', 'published_dt'], 'safe'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'description' => 'Description',
+            'content' => 'Content',
+            'status' => 'Status',
+            'created_dt' => 'Created Dt',
+            'updated_dt' => 'Updated Dt',
+            'published_dt' => 'Published Dt',
+            'comments' => 'Comments',
+        ];
+    }
+}
