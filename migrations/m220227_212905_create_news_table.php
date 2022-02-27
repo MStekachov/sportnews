45b63327bd6eb7c437168ecaff7e160ac3e6b05c
Михаил Стекачёв <mstekachov@gmail.com>
2022-02-28 02:21:37 +0300

    applied news migration

--- migrations/m220227_212905_create_news_table.php
+++ migrations/m220227_212905_create_news_table.php
@@ -14,6 +14,14 @@ class m220227_212905_create_news_table extends Migration
     {
         $this->createTable('{{%news}}', [
             'id' => $this->primaryKey(),
+            'title' => $this->string()->notNull(),
+            'description' => $this->text()->notNull(),
+            'content' => $this->text()->notNull(),
+            'status' => $this->integer(1)->defaultValue(0),
+            'created_dt' => $this->dateTime()->defaultExpression('CURRENT_TIMESTAMP'),
+            'updated_dt' => $this->dateTime()->null(),
+            'published_dt' => $this->dateTime()->null(),
+            'comments' => $this->text()->null()
         ]);
     }
 
