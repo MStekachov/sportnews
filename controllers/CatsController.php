35f2118a20a72c58659da3949dd40ed534d33900
Михаил Стекачёв <mstekachov@gmail.com>
2022-03-01 21:21:49 +0300

    Realized cats CRUD

--- controllers/CatsController.php
+++ controllers/CatsController.php
@@ -3,13 +3,57 @@
 namespace app\controllers;
 
 use app\models\Cats;
+use yii\data\ActiveDataProvider;
+use Yii;
+use yii\web\Request;
 
 class CatsController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        $catsList = Cats::find()->orderBy(['parent_id'=>SORT_ASC])->asArray(true)->all();
-        return $this->render('index', ['cats' =>  $catsList]);
+        $query = Cats::find()->all();
+
+        $provider = new ActiveDataProvider([
+            'query' => $query,
+            'pagination' => [
+                'pageSize' => 5,
+            ],
+        ]);
+        return $this->render('index', ['dataProvider' => $provider]);
+    }
+
+    public function actionView($id)
+    {
+        $model = Cats::findOne($id);
+        return $this->render('view', ['model' => $model]);
+    }
+
+    public function actionCreate()
+    {
+        $model = new Cats();
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            Yii::$app->session->setFlash('success', 'Запись  успешно создана');
+            return $this->redirect(['cats/index']);
+        }
+        return $this->render('create', ['model' => $model]);
+    }
+
+    public function actionUpdate($id)
+    {
+        $model = Cats::findOne($id);
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            Yii::$app->session->setFlash('success', 'Запись  успешно изменена');
+            return $this->redirect(['cats/index']);
+        }
+        return $this->render('update', ['model' => $model]);
+    }
+
+    public function actionDelete($id)
+    {
+        $model = Cats::findOne($id);
+        $model->delete();
+        Yii::$app->session->setFlash('success', 'Запись  успешно удалена');
+        return $this->redirect(['cats/index']);
     }
 
 }
